15da6c2d5b4e058ca35c1a4b40f9065b26c44597
SamOstrander <94343313+SamOstrander@users.noreply.github.com>
2021-11-15 00:34:11 -0500

    Add files via upload

---
+++ TreeGenerator.java
@@ -0,0 +1,183 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Point2D;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.swing.*;    
+
+class TreeCanvas extends JComponent {
+    //
+    List<Point2D.Double> point_list;  //
+
+    int window_width;
+    int window_height;
+    int offset_x;
+    int offset_y;
+
+    public void set_window_size(int w, int h)
+    {
+        //updates window size and offset vals for use in the paint function.
+        window_width = w;
+        window_height = h;
+        offset_x = window_width/2;
+        offset_y = window_height-42;
+    }
+
+    public void set_list(List<Point2D.Double> l)
+    {
+        point_list = l;
+        repaint();
+    }
+
+    public void paint(Graphics g)
+    {   
+            //I'll need to add adjustments here. little offsets to flip the y as well as start drawing from the bottom center of the screen. 
+        if (point_list == null)
+        {
+            return;
+        }
+        int sz = point_list.size();
+        
+        for (int i=0; i < point_list.size(); ++i)  //draws lines connecting the points potulating vertex_list. (last point is connected to first point)
+            g.drawLine((int)Math.round(point_list.get(i).getX())+offset_x,-1*(int)Math.round(point_list.get(i).getY())+offset_y,(int)Math.round(point_list.get((i+1)%sz).getX())+offset_x,-1*(int)Math.round(point_list.get((i+1)%sz).getY())+offset_y);
+    }
+}
+
+public class TreeGenerator {
+    //Generates a randomized 2d tree, first as a series of lines, which are then converted to a tree outline.
+
+    static List<Point2D.Double> tree_verticies = new ArrayList<Point2D.Double>();
+
+    static TreeLimb base;
+    static TreeCanvas tCanv;
+
+    static double max_ang = 60; //base range of angles
+    static double min_ang = 55; //
+
+    static double max_height;
+    
+    public static void main(String[] args)
+    {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        int window_h = (int)screenSize.getHeight();
+        int window_w = window_h;
+
+        max_height = (double)window_h-240;  //used in capping height of tree.
+
+        tCanv = new TreeCanvas();
+
+        JFrame window = new JFrame("Tree Generator");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds((int)(window_w/6), 0, (int)(5*window_w/6), (int)(5*window_h/6));
+        tCanv.set_window_size((int)window.getSize().getWidth(),(int)window.getSize().getHeight());
+
+        JButton button = new JButton("Generate Tree");
+        button.setBounds(50,50,150,100);
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                reset_tree();
+                Timer t1 = new Timer();
+                
+                t1.scheduleAtFixedRate(new TimerTask() {
+                    TreeLimb b1 = TreeGenerator.base;
+                    public void run(){
+                        TreeGenerator.clear_list();
+                        TreeGenerator.update_tree(TreeGenerator.base, 2.0);
+                        TreeGenerator.tCanv.set_list(TreeGenerator.tree_verticies);
+                        if(b1 != base || TreeGenerator.base.get_length() >= TreeGenerator.max_height)
+                            cancel();
+                            return;
+                    }
+                },0,1000/200);
+            }
+        });
+        window.add(button);
+        window.getContentPane().add(tCanv);
+        window.setVisible(true);
+
+        //TreeLimb base
+        base = new TreeLimb(new Point2D.Double(0,0),90.0,new Random().nextInt(2),new TreeLimb(new Point2D.Double(0,0),0.0,0,null));
+        base.set_subbranch(0);
+
+        tree_verticies.clear();
+        tCanv.set_list(tree_verticies); //update vertex list.
+    }
+
+    
+    public static void clear_list()
+    {
+        tree_verticies.clear();
+    }
+
+    public static void reset_tree()
+    {
+        base = new TreeLimb(new Point2D.Double(0,0),90.0,new Random().nextInt(2),new TreeLimb(new Point2D.Double(0,0),0.0,0,null));//new TreeLimb(new Point2D.Double(0,0),0.0,0,null));
+        base.set_subbranch(0);  //bugfix
+    }
+
+    public static void update_tree(TreeLimb limb, double time)
+    {   
+        limb.grow_by_time(time);
+
+        //add base left point.
+        //meaning, intersection between limb's left line, and parent's [parent_side] line.
+        tree_verticies.add(limb.get_intersection(0));    //takes in side (0 or 1), and uses parent side.
+
+        if(limb.is_branching() && limb.get_length() >= limb.get_nexttime())
+        {
+            //if can_branch, and length passed grow threshold, then create new branch.
+
+            //modifying a randomized element to get an angle shifting for a new branch. altered by both length of branch, as well as how deep a sub_branch it is.
+            double rn = new Random().nextDouble() * (max_ang - min_ang) + min_ang - ((limb.get_subbranch()-1)*20) - (15*limb.get_length()/max_height);  //randomize angle between bounds.
+            
+            double next_ang = ((double)limb.get_nextside()*2 - 1)*rn + limb.get_angle();
+            Point2D.Double pt = new Point2D.Double(limb.get_position().getX() + limb.get_length()*Math.cos(Math.toRadians(limb.get_angle())),limb.get_position().getY() + limb.get_length()*Math.sin(Math.toRadians(limb.get_angle())));
+
+            limb.add_branch(new TreeLimb(pt, next_ang, limb.get_nextside(), limb), limb.get_nextside());
+            limb.update_next();
+        }
+
+        for (int i=0; i < limb.left_branches.size(); ++i)
+        {
+            //call update
+            update_tree(limb.left_branches.get(i),time);
+        }
+        
+        //add tip point
+        tree_verticies.add(limb.get_tip()); //generated using angle, length and base point.
+
+        //recursively loops through right branches
+        for (int i=0; i < limb.right_branches.size(); ++i)
+        {
+            //call update
+            update_tree(limb.right_branches.get(i),time);
+        }
+
+        //add base right point
+        tree_verticies.add(limb.get_intersection(1));
+
+    }
+
+    public static void print_points(List<Point2D.Double> list)
+    {
+        //print ordered list of points which create the tree.
+
+        if(list == null)
+        {
+            return;
+        }
+        int sz = list.size();
+        for(int i=0;i<sz;++i)
+        {
+            list.get(i).getX();
+            System.out.println(list.get(i).getX() + " , " + list.get(i).getY());
+        }
+        return;
+    }
+}
---
+++ TreeLimb.java
@@ -0,0 +1,289 @@
+import java.util.LinkedList;
+import java.awt.geom.Point2D;
+import java.util.Random;
+
+public class TreeLimb {
+    //uses a line to create a limb
+    
+    //double creation_time;   //use this to calculate length on draw.
+    Point2D.Double position;        //position of base of branch relative to base of tree. 
+    double length;
+    double angle;
+    double half_branchtip_angle = .75;   //angle of tip of branch triangle /2 (in degrees)
+    double max_angle_adjustment = 1.3;  //modifier bound for tip angle randomization.
+    //might need to store components of the angle function here, instead of just the angle, as i'll need to recalculate it every step otherwise.
+
+        //data for the lines, constants rb and lb will  be adjusted as length increases over time.
+    double cm;  //center slope
+    double cb;  //center constant
+    double lm;  //left slope
+    double lb;  //left constant
+    double rm;  //right slope
+    double rb;  //right constant
+
+    //should i use this type of variable?
+    double next_branch_timer; //I could almost just tie this to a point along the branch, rather than a time. 
+
+    int sub_branch;
+    //int last_side;   //0 for left, 1 for right. (last side branch spawned from)
+    int parent_side; //0 for left, 1 for right. the side of the parent branch this limb was spawned from. used for determining the line to use for the intersection point finding. 
+    
+        //next time
+    boolean isBranching = true; //should this branch trigger creation of additional sub branches.
+    int next_side;      //next branch spawned from [next_side] side. (left-0, right-1)
+    double next_time;   //compared against length to determine if it's time to grow another branch. 
+    
+    TreeLimb parent;    //ptr to parent.
+
+    //next_time max and min.
+    double nt_max = 120;
+    double nt_min = 100;
+
+    LinkedList<TreeLimb> left_branches;// = new LinkedList<>();     //new branches added to end.
+    LinkedList<TreeLimb> right_branches;// = new LinkedList<>();    //new branches added to front.
+
+    public TreeLimb(Point2D.Double pos, double ang, int prev, TreeLimb par)
+    {
+        position = pos;
+        angle = ang;    //angle of branch
+        half_branchtip_angle += new Random().nextDouble()*max_angle_adjustment;   //adding randomization to branch width.
+        
+        length = 1; //base length of branch
+        calculate_lines();
+
+        sub_branch = 0;
+        if(par != null)
+        {
+            sub_branch = par.get_subbranch()+1;
+        }
+        else
+        {
+            //for the dummy branch.
+            //ensuring that the bottom of the tree is straight.
+            //however, this might be unneccessary due to length being 0, meaning there is no angle adjustment due to size.
+            lb = cb;
+            rb = cb;
+            lm = cm;
+            rm = cm;
+        }
+
+        if(sub_branch >= 3) //no 4th sub_branches
+        {
+            isBranching = false;
+        }
+
+        parent_side = prev;   //0 for left, 1 for right. growing from this [parent_side] of parent branch.
+        parent = par;    //ptr to parent.
+
+                                                        //Ordering is deliberate for conversion of branch lines to Points.
+        left_branches = new LinkedList<TreeLimb>();     //new branches added to end.
+        right_branches  = new LinkedList<TreeLimb>();   //new add to front.
+
+        next_side = new Random().nextInt(2);
+        
+        if(isBranching) //sub_branch < 3)  //Stop branching 3 deep.
+            update_next();
+    }
+
+    public void add_branch(TreeLimb branch,int side)
+    {
+        if(side == 0)
+            left_branches.addLast(branch);      //add to left_branches
+        else
+            right_branches.addFirst(branch);    //add to right_branches
+    }
+
+    public Point2D.Double get_position()
+    {
+        //returns position as Point2D(double x, double y)
+        return position;
+    }
+
+    public void set_angle(double new_angle)
+    {
+        //Sets angle of branch. would only be used at creation.
+        angle = new_angle;
+    }
+
+    public double get_angle()
+    {
+        //Returns angle of branch in degree
+        return angle;
+    }
+
+    public void set_length(double len)
+    {
+        //Sets length and updates lines to reflect the change. Probably low cohesion.
+        length = len;
+        update_line();
+    }
+    public double get_length()
+    {
+        //Returns length of branch.
+        return length;
+    }
+
+    public double get_nexttime()
+    {
+        //Returns length at which new branch spawn will be triggered
+        return next_time;
+    }
+    public int get_nextside()
+    {
+        //Returns the side that the next branch will grow from.
+        return next_side;
+    }
+
+    public void update_next()
+    {
+        //updates next_side and next_time
+        next_time = length + new Random().nextDouble() * (nt_max-nt_min) + nt_min + sub_branch*10;
+        next_side = (new Random().nextDouble() > .90) ? next_side:(next_side+1)%2; //.7 chance that next side is opposite last.
+    }
+
+    public boolean is_branching()
+    {
+        return isBranching;
+    }
+
+    public int get_subbranch()
+    {
+        return sub_branch;
+    }
+
+    public void set_subbranch(int sub)
+    {
+        //sets sub_branch. used for base branch(trunk), as all others are generated off of their parent's value.
+        sub_branch = sub;
+    }
+
+    public void update_line()
+    {
+        //call when length has been changed (or upon creation of the branch)
+
+            //get new tip point.
+        
+        Point2D.Double branch_tip = get_tip(); // new Point2D.Double(position.getX() + length*Math.cos(Math.toRadians(angle)),position.getY() +length*Math.sin(Math.toRadians(angle)));
+            //Update line constants. Slopes remain unchanged.
+        lb = branch_tip.getY() - branch_tip.getX()*lm;
+
+        rb = branch_tip.getY() - branch_tip.getX()*rm;
+
+        //System.out.println(lb);
+        //System.out.println(rb);
+    }
+
+    public void grow_by_time(double time)
+    {
+        //pass a time val which determines growth amount.
+        double sub_mod = ((double)sub_branch + 1);
+        set_length(get_length()+time/sub_mod);//sub_branch);  //just add flat for now.
+    }
+
+    public void calculate_lines()
+    {
+        //Solves for 3 lines and stores their values as class members. 
+        //This function is called once per unique branchLimb object within the class constructor.
+        //First is the central line, or the branch skeleton.
+        //Left and right lines form the outline of the branch and are used when creating the verticies for drawing the tree.
+
+        //base point is position along parent skeleton line that this branch comes off of. 
+        //that is stored at creation as [position].
+        
+        //Central line or branch skeleton.
+        //constant = startPoint_Y - (tan ( angle) * startPoint_X )
+        cm = Math.tan(Math.toRadians(angle));
+        cb = position.getY() - position.getX()*cm;
+        
+        //x = r cos(theta) where r is length and theta is angle from origin to target point.
+            //use central line to get branch_tip.
+        Point2D.Double branch_tip = get_tip();//new Point2D.Double(position.getX() + length*Math.cos(Math.toRadians(angle)),position.getY() + length*Math.sin(Math.toRadians(angle)));
+
+        //branch_tip used to create 2 lines. angle offset by have of branch tip angle
+        lm = Math.tan(Math.toRadians(angle + half_branchtip_angle));
+        lb = branch_tip.getY() - branch_tip.getX()*lm;
+
+        rm = Math.tan(Math.toRadians(angle - half_branchtip_angle));
+        rb = branch_tip.getY() - branch_tip.getX()*rm;
+    }
+
+    public Point2D.Double get_intersection(int side)//double m1, double b1, double m2, double b2)
+    {
+        //returns intersection point between current limb's [side] line, and parent limb's [parent_side] line.
+        double m1 = side < 1 ? lm : rm;
+        double m2 = parent_side < 1 ? parent.get_leftslope() : parent.get_rightslope();
+        double b1 = side < 1 ? lb : rb;
+        double b2 = parent_side < 1 ? parent.get_leftconstant() : parent.get_rightconstant();
+        
+        //System.out.println("y1="+m1+"x1+"+b1+"|y2="+m2+"x2+"+b2);
+
+        if(m1 == m2)
+        {
+            //slopes are equivalent aka parallel lines. this shoudn't happen. 
+            System.out.println("Parallel slopes, meaning either 0 intersections points or infinite.");
+            return null;
+        }
+        else
+            //System.out.println("INTERSECTION: " + new Point2D.Double((-1*b2 - -1*b1) / (m1*-1 - m2*-1),(b1*m2 - b2*m1) / (m1*-1 - m2*-1)));
+            return new Point2D.Double((-1*b2 - -1*b1) / (m1*-1 - m2*-1),(b1*m2 - b2*m1) / (m1*-1 - m2*-1));
+            //return new Point2D.Double((-b2 + b1)/(-m1 + m2),(b1*m2)/(-m1 + m2));
+    }
+
+    public Point2D.Double get_tip()
+    {
+        return new Point2D.Double(position.getX() + length*Math.cos(Math.toRadians(angle)),position.getY() + length*Math.sin(Math.toRadians(angle)));
+    }
+
+    public void set_nextside(int side)
+    {
+        next_side = side;
+    }
+
+    public void set_nexttimer(double timer)
+    {
+        next_time = timer;
+    }
+
+    public TreeLimb get_parent()
+    {
+        return parent;
+    }
+
+    public int get_parent_side()
+    {
+        //returns parent_size, 0 or 1.  0-left, 1-right.
+        return parent_side;
+    }
+
+    //getters. will be used to grab a parent branch's line for finding intersection points. 
+    public double get_leftslope()
+    {
+        return lm;
+    }
+
+    public double get_leftconstant()
+    {
+        return lb;
+    }
+
+    public double get_rightslope()
+    {
+        return rm;
+    }
+
+    public double get_rightconstant()
+    {
+        return rb;
+    }
+
+    public double get_centerslope()
+    {
+        return cm;
+    }
+
+    public double get_centerconstant()
+    {
+        return cb;
+    }
+
+}
